eeb8540ec73417940e1e38c07281109c34e199d8
David Yell <dyell@ukwebmedia.com>
2015-06-30 09:58:20 +0100

    PHPCS fixes, added dev dependancies

--- src/Controller/Component/SeoComponent.php
+++ src/Controller/Component/SeoComponent.php
@@ -11,93 +11,95 @@ use Cake\Event\EventListenerInterface;
  * @author David Yell <neon1024@gmail.com>
  */
 
-class SeoComponent extends Component implements EventListenerInterface {
+class SeoComponent extends Component implements EventListenerInterface
+{
 
-	/**
-	 * Store component settings
-	 *
-	 * @var array
-	 */
-	protected $_defaultConfig = [
-		'viewVar' => 'content', // Name of the view variable being used in views
-		'model' => 'Content', // Model containing the fields
-		'fields' => [
-			'title' => 'seo_title',
-			'description' => 'seo_description',
-			'keywords' => 'seo_keywords'
-		],
-		'noSeoPrefix' => ['admin'], // Any routing prefixes you do not SEO
-		'defaults' => [
-			'title' => 'The homepage | My Awesome Website',
-			'description' => 'Find out about how awesome I am by reading my website',
-			'keywords' => 'my, website, is, totally, awesome'
-		]
-	];
-	
-	/**
-	 * Create the class
-	 * 
-	 * @param array $config The component configuration array
-	 * @return void
-	 */
-	public function initialize(array $config)
-	{
-		if (!in_array($controller->request->prefix, $this->settings['noSeoPrefix'])) {
-			$controller->getEventManager()->attach($this);
-		}
-	}
+    /**
+     * Store component settings
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'viewVar' => 'content', // Name of the view variable being used in views
+        'model' => 'Content', // Model containing the fields
+        'fields' => [
+            'title' => 'seo_title',
+            'description' => 'seo_description',
+            'keywords' => 'seo_keywords'
+        ],
+        'noSeoPrefix' => ['admin'], // Any routing prefixes you do not SEO
+        'defaults' => [
+            'title' => 'The homepage | My Awesome Website',
+            'description' => 'Find out about how awesome I am by reading my website',
+            'keywords' => 'my, website, is, totally, awesome'
+        ]
+    ];
+    
+    /**
+     * Create the class
+     *
+     * @param array $config The component configuration array
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        if (!in_array($controller->request->prefix, $this->settings['noSeoPrefix'])) {
+            $controller->getEventManager()->attach($this);
+        }
+    }
 
-	/**
-	 * List of callable functions which are attached to system events
-	 *
-	 * @return array
-	 */
-	public function implementedEvents()
-	{
-		return array(
-			'View.beforeLayout' => 'writeSeo'
-		);
-	}
+    /**
+     * List of callable functions which are attached to system events
+     *
+     * @return array
+     */
+    public function implementedEvents()
+    {
+        return [
+            'View.beforeLayout' => 'writeSeo'
+        ];
+    }
 
-/**
- * Inject the seo data into the view
- *
- * @param CakeEvent $event Event instance
- * @return void
- */
-	public function writeSeo(CakeEvent $event) {
-		if (!empty($event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['title']])) {
-			$seoTitle = $event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['title']];
-			$event->subject()->viewVars['title_for_layout'] = $seoTitle;
-		}
+    /**
+     * Inject the seo data into the view
+     *
+     * @param CakeEvent $event Event instance
+     * @return void
+     */
+    public function writeSeo(CakeEvent $event)
+    {
+        if (!empty($event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['title']])) {
+            $seoTitle = $event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['title']];
+            $event->subject()->viewVars['title_for_layout'] = $seoTitle;
+        }
 
-		if (!empty($event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['description']])) {
-			$seoDescription = $event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['description']];
-			$event->subject()->Html->meta(
-				'description',
-				$seoDescription,
-				['block' => 'meta']
-			);
-		}
+        if (!empty($event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['description']])) {
+            $seoDescription = $event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['description']];
+            $event->subject()->Html->meta(
+                'description',
+                $seoDescription,
+                ['block' => 'meta']
+            );
+        }
 
-		if (!empty($event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['keywords']])) {
-			$seoKeywords = $event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['keywords']];
-			$event->subject()->Html->meta(
-				'keywords',
-				$seoKeywords,
-				['block' => 'meta']
-			);
-		}
+        if (!empty($event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['keywords']])) {
+            $seoKeywords = $event->subject()->viewVars[$this->settings['viewVar']][$this->settings['model']][$this->settings['fields']['keywords']];
+            $event->subject()->Html->meta(
+                'keywords',
+                $seoKeywords,
+                ['block' => 'meta']
+            );
+        }
 
-		// If no values can be found, fall back to the defaults
-		if (empty($seoTitle)) {
-			$event->subject()->viewVars['title_for_layout'] = $this->settings['defaults']['title'];
-		}
-		if (empty($seoDescription)) {
-			$event->subject()->Html->meta('description', $this->settings['defaults']['description'], ['block' => 'meta']);
-		}
-		if (empty($seoKeywords)) {
-			$event->subject()->Html->meta('keywords', $this->settings['defaults']['keywords'], ['block' => 'meta']);
-		}
-	}
+        // If no values can be found, fall back to the defaults
+        if (empty($seoTitle)) {
+            $event->subject()->viewVars['title_for_layout'] = $this->settings['defaults']['title'];
+        }
+        if (empty($seoDescription)) {
+            $event->subject()->Html->meta('description', $this->settings['defaults']['description'], ['block' => 'meta']);
+        }
+        if (empty($seoKeywords)) {
+            $event->subject()->Html->meta('keywords', $this->settings['defaults']['keywords'], ['block' => 'meta']);
+        }
+    }
 }
--- src/View/Helper/SeoHelper.php
+++ src/View/Helper/SeoHelper.php
@@ -9,64 +9,63 @@ use Cake\View\Helper;
  *
  * @author David Yell <neon1024@gmail.com>
  */
-class SeoHelper extends Helper {
+class SeoHelper extends Helper
+{
 
-	/**
-	 * Keep a list of all the controllers which generate paginated lists of items
-	 *
-	 * @var array
-	 */
-	public $paginatedControllers = array();
+    /**
+     * Keep a list of all the controllers which generate paginated lists of items
+     *
+     * @var array
+     */
+    public $paginatedControllers = array();
 
-	/**
-	 * When using a paginated set of pages a link tag is used to show which pages
-	 * are previous and next in the paginated series
-	 *
-	 * @param string $controller The name of the controller
-	 * @return string
-	 */
-	public function pagination($controller)
-	{
-		if (in_array($controller, $this->paginatedControllers)) {
+    /**
+     * When using a paginated set of pages a link tag is used to show which pages
+     * are previous and next in the paginated series
+     *
+     * @param string $controller The name of the controller
+     * @return string
+     */
+    public function pagination($controller)
+    {
+        if (in_array($controller, $this->paginatedControllers)) {
+            $className = Inflector::classify($controller);
 
-			$className = Inflector::classify($controller);
+            if (isset($this->request->params['paging'][$className])) {
+                $prev = "<link rel='prev' href='" . Router::url(array('controller' => $controller, 'action' => 'index', 'page' => $this->request->params['paging'][$className]['page'] - 1)) . "'>";
+                $next = "<link rel='next' href='" . Router::url(array('controller' => $controller, 'action' => 'index', 'page' => $this->request->params['paging'][$className]['page'] + 1)) . "'>";
 
-			if (isset($this->request->params['paging'][$className])) {
-				$prev = "<link rel='prev' href='" . Router::url(array('controller' => $controller, 'action' => 'index', 'page' => $this->request->params['paging'][$className]['page'] - 1)) . "'>";
-				$next = "<link rel='next' href='" . Router::url(array('controller' => $controller, 'action' => 'index', 'page' => $this->request->params['paging'][$className]['page'] + 1)) . "'>";
+                if ($this->request->params['paging'][$className]['prevPage'] === false) { // page 1
+                    return $next;
+                } elseif ($this->request->params['paging'][$className]['nextPage'] === false) {
+                    return $prev;
+                } else {
+                    return $next . $prev;
+                }
+            }
+        }
+    }
 
-				if ($this->request->params['paging'][$className]['prevPage'] === false) { // page 1
-					return $next;
-				} elseif ($this->request->params['paging'][$className]['nextPage'] === false) {
-					return $prev;
-				} else {
-					return $next . $prev;
-				}
-			}
-		}
-	}
+    /**
+     * Output a canonical tag for content with multiple pages or other dynamic data
+     *
+     * @param string $controller The name of the controller
+     * @param string $action The name of the action
+     * @return string
+     */
+    public function canonical($controller, $action)
+    {
+        $url = preg_replace("/page:[0-9]+/", '', $this->here);
 
-	/**
-	 * Output a canonical tag for content with multiple pages or other dynamic data
-	 *
-	 * @param string $controller The name of the controller
-	 * @param string $action The name of the action
-	 * @return string
-	 */
-	public function canonical($controller, $action)
-	{
-		$url = preg_replace("/page:[0-9]+/", '', $this->here);
+        if (isset($this->request->params['sort'])) {
+            $url = str_replace($this->request->params['sort'], '', $url);
+        }
+        if (isset($this->request->params['dir'])) {
+            $url = str_replace($this->request->params['dir'], '', $url);
+        }
+        $url = rtrim($url, '/');
+        $url = Router::fullbaseUrl() . $url;
 
-		if (isset($this->request->params['sort'])) {
-			$url = str_replace($this->request->params['sort'], '', $url);
-		}
-		if (isset($this->request->params['dir'])) {
-			$url = str_replace($this->request->params['dir'], '', $url);
-		}
-		$url = rtrim($url, '/');
-		$url = Router::fullbaseUrl() . $url;
-
-		return "<link rel='canonical' href='$url'>";
-	}
-
+        return "<link rel='canonical' href='$url'>";
+    }
 }
